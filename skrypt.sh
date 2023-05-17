89b0ab6ff2f71435c9a497a55f1e105605521d3a
„Liv700” <email@dsw.edu.pl>
2023-05-17 21:06:31 +0200

    myślnik 4

--- skrypt.sh
+++ skrypt.sh
@@ -3,11 +3,23 @@
 if [ $1 == "--date" ]; then
 	echo "Dzisiejsza data to:" $(date +%F)
 elif [ $1 == "--logs" ]; then
-	for i in {1..100}
-	do
-		touch log$i.txt
-		echo "Nazwa pliku to:" log$i.txt>log$i.txt
-		echo "Plik utworzony przez skrypt 'skrypt.sh'">>log$i.txt
-		echo "Data utworzenia:" $(date +%F)>>log$i.txt
-	done
+	if [ $# == 1 ]; then	
+		for i in {1..100}
+		do
+			touch log$i.txt
+			echo "Nazwa pliku to:" log$i.txt>log$i.txt
+			echo "Plik utworzony przez skrypt 'skrypt.sh'">>log$i.txt
+			echo "Data utworzenia:" $(date +%F)>>log$i.txt
+		done
+	elif [ $# == 2 ]; then
+		var=$2
+		for i in {1..$var}
+		do
+			touch log$i.txt
+			echo "Nazwa pliku to:" log$i.txt>log$i.txt
+                        echo "Plik utworzony przez skrypt 'skrypt.sh'">>log$i.txt
+                        echo "Data utworzenia:" $(date +%F)>>log$i.txt
+		done
+	fi
 fi
+
