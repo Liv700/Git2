76a17354a6c267a857f658149ad3e62a21172d05
„Liv700” <email@dsw.edu.pl>
2023-05-17 20:33:43 +0200

    myślnik 3-ci

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,12 @@
 
 if [ $1 == "--date" ]; then
 	echo "Dzisiejsza data to:" $(date +%F)
+elif [ $1 == "--logs" ]; then
+	for i in {1..100}
+	do
+		touch log$i.txt
+		echo "Nazwa pliku to:" log$i.txt>log$i.txt
+		echo "Plik utworzony przez skrypt 'skrypt.sh'">>log$i.txt
+		echo "Data utworzenia:" $(date +%F)>>log$i.txt
+	done
 fi
